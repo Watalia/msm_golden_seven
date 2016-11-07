c89e1c2994846b4515e7d189d43e67dd6159beb8
Natalia Botia <nbotia@users.noreply.github.com>
2016-11-07 15:30:39 -0600

    Last changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  
+
+  get('/', {:controller => 'movies', :action => 'index'})
+  # Routes for the Director resource:
+  # Read
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
+  # Delete
+  get('/delete_director/:id', {:controller=> 'directors', :action => 'destroy'})
+  # We now create a new director
+  get('/directors/new_form', {:controller => 'directors', :action => 'new_form'})
+  get('/create_director', {:controller => 'directors', :action => 'create_row'})
+  # Update
+  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+
 end
